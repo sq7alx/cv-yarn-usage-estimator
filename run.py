85c7a8d320683c27f197e704a856966acdad530b
sq7alx <sq7alx@gmail.com>
2025-09-29 21:37:28 +0200

    update: deploy config

--- run.py
+++ run.py
@@ -1,4 +1,6 @@
+import os
 from app.yarn_calculator import app
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
